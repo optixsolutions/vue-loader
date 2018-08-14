e02699241aa619b3f67be067b23af16a48b811f7
Richard Moore <r1.moore@gmail.com>
2018-08-14 09:44:43 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Vue Loader
+
+A plugin to handle the loading state within your Vue applications
+
+## Installation
+
+```bash
+npm install @optix/vue-loader --save
+yarn add @optix/vue-loader
+```
+
+```javascript
+import Vue from 'vue';
+import VueLoader from '@optix/vue-loader';
+
+Vue.use(VueLoader);
+```
+
+## Usage
+
+Accessing the loader object:
+
+```javascript
+// Globally
+Vue.loader.start('example');
+
+// On an instance
+this.$loader.start('example');
+```
+
+### Methods
+
+* start(item)
+* stop(item)
+* clear()
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ src/index.js
@@ -0,0 +1,57 @@
+export default function install(Vue, options = {}) {
+    
+    if (! options.hasOwnProperty('store')) {
+        throw new Error('Please provide vuex store.');
+    }
+
+    options.store.registerModule('loader', {
+        namespaced: true,
+
+        state: {
+            loading: []
+        },
+
+        getters: {
+            isLoading: state => state.loading.length > 0
+        },
+
+        mutations: {
+            start(state, item) {
+                state.loading.push(item);
+            },
+        
+            stop(state, item) {
+                let index = state.loading.indexOf(item);
+        
+                if (index != -1) {
+                    state.loading.splice(index, 1);
+                }
+            },
+        
+            clear(state) {
+                state.loading = [];
+            }
+        }
+    });
+
+    Vue.loader = {
+        start(item) {
+            options.store.commit('loader/start', item);
+        },
+
+        stop(item) {
+            options.store.commit('loader/stop', item);
+        },
+
+        clear() {
+            options.store.commit('loader/clear');
+        }
+    };
+
+    Object.defineProperty(Vue.prototype, '$loader', {
+        get() {
+            return Vue.loader;
+        }
+    });
+
+}
